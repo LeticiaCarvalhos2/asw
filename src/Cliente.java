cdcc64cc6e0515a100b36413086d87e4ff24edd2
Andre Willik Valenti <awvalenti@gmail.com>
2019-04-03 15:58:41 -0300

    Organizando melhor try-with-resources

--- src/Cliente.java
+++ src/Cliente.java
@@ -8,29 +8,27 @@ public class Cliente {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		System.out.println("Cliente: iniciando");
-		
+
 		String endereco = "localhost";
 		int porta = 3233;
-		
-		try (Socket cliente = new Socket(endereco, porta)) {
-			System.out.println("Cliente: conectado com sucesso");
-			
-			try (Scanner leitorTeclado = new Scanner(System.in);
-					PrintStream saidaServidor = new PrintStream(cliente.getOutputStream())) {
-				
-				int opcaoInt;
-				do {
-					System.out.println("Selecione\n 0 - Pedra \n 1 - Papel\n 2 - Tesoura");
-					opcaoInt = Integer.parseInt(leitorTeclado.nextLine());
-				} while (opcaoInt < 0 || opcaoInt >= 3);
 
-				saidaServidor.println(Opcao.valueOf(opcaoInt));
-				
-				try (Scanner msg = new Scanner(cliente.getInputStream())) {
-					System.out.println(Formatador.escolhaMaquina(Opcao.valueOf(msg.nextLine())));
-					System.out.println(Formatador.resultado(Integer.parseInt(msg.nextLine())));
-				}
-			}
+		try (Socket cliente = new Socket(endereco, porta);
+				Scanner leitorTeclado = new Scanner(System.in);
+				PrintStream saidaServidor = new PrintStream(cliente.getOutputStream());
+				Scanner leituraServidor = new Scanner(cliente.getInputStream())) {
+
+			System.out.println("Cliente: conectado com sucesso");
+
+			int opcaoInt;
+			do {
+				System.out.println("Selecione\n 0 - Pedra \n 1 - Papel\n 2 - Tesoura");
+				opcaoInt = Integer.parseInt(leitorTeclado.nextLine());
+			} while (opcaoInt < 0 || opcaoInt >= 3);
+
+			saidaServidor.println(Opcao.valueOf(opcaoInt));
+
+			System.out.println(Formatador.escolhaMaquina(Opcao.valueOf(leituraServidor.nextLine())));
+			System.out.println(Formatador.resultado(Integer.parseInt(leituraServidor.nextLine())));
 		}
 	}
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -17,20 +17,18 @@ public class Servidor {
 		try (ServerSocket servidor = new ServerSocket(porta)) {
 			System.out.println("Servidor: ouvindo porta " + porta);
 			
-			try (Socket cliente = servidor.accept()) {
+			try (Socket cliente = servidor.accept();
+					Scanner leitorJogador = new Scanner(cliente.getInputStream());
+					PrintStream saidaJogador = new PrintStream(cliente.getOutputStream())) {
 				System.out.println("Servidor: conexão de cliente em " + cliente.getInetAddress().getHostAddress());
 				
-				try (Scanner leitorJogador = new Scanner(cliente.getInputStream())) {
-					Opcao opcaoJogador = Opcao.valueOf(leitorJogador.nextLine());
-					Opcao opcaoMaquina = Opcao.valueOf(new Random().nextInt(3));
-					
-					System.out.printf("Servidor: jogador %s, maquina %s\n", opcaoJogador, opcaoMaquina);
-					
-					try (PrintStream saidaJogador = new PrintStream(cliente.getOutputStream())) {
-						saidaJogador.println(opcaoMaquina);
-						saidaJogador.println(Opcao.comparar(opcaoJogador, opcaoMaquina));
-					}
-				}
+				Opcao opcaoJogador = Opcao.valueOf(leitorJogador.nextLine()),
+						opcaoMaquina = Opcao.valueOf(new Random().nextInt(3));
+				
+				System.out.printf("Servidor: jogador %s, maquina %s\n", opcaoJogador, opcaoMaquina);
+				
+				saidaJogador.println(opcaoMaquina);
+				saidaJogador.println(Opcao.comparar(opcaoJogador, opcaoMaquina));
 			}
 		}
 	}
